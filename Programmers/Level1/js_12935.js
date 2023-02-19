13c5fae84bfc228768cb1f8a6430e58788794323
yunii23 <67429918+yunii23@users.noreply.github.com>
2023-02-19 15:09:58 +0900

    레벨1 제일 작은 수 제거하기 완료

--- Programmers/Level1/js_12935.js
+++ Programmers/Level1/js_12935.js
@@ -4,16 +4,15 @@ let arr = [10];
 
 let answer = [];
 
-if (answer.length == 0 || answer.length == 1) answer.push(-1);
-else {
-    let min = Math.min.apply(null, arr);
+let min = Math.min.apply(null, arr);
 
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] == min) arr.splice(i, 1);
-    }
-
-    answer = arr;
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i] == min) arr.splice(i, 1);
 }
+
+answer = arr;
+
+if (answer.length == 0 || answer.length == 1) answer.push(-1);
 
 console.log(arr);
 console.log(answer);
