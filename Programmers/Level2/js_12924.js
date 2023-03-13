37da7f28cab8fe6c9f6c064d57efb61553b723e0
yunii23 <67429918+yunii23@users.noreply.github.com>
2023-03-14 00:23:14 +0900

    레벨2 숫자의 표현 완료

---
+++ Programmers/Level2/js_12924.js
@@ -0,0 +1,20 @@
+// 숫자의 표현
+
+let n = 15;
+let answer = 0;
+
+for(let i=1; i<=n; i++) {
+    let sum = 0;
+
+    for(let j=i; j<=n; j++) {
+        sum += j;
+
+        if(sum == n) {
+            answer ++;
+            break;
+        }
+        else if(sum > n) break;
+    }
+}
+
+console.log(answer);
